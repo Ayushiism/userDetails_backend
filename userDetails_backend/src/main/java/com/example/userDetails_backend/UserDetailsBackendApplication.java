04362a0cfc1bae8a5d8dc811e89220033260f2df
Ayushiiism <ayushtripathy08@gmail.com>
2023-10-20 14:06:05 +0530

    first commit

---
+++ userDetails_backend/src/main/java/com/example/userDetails_backend/UserDetailsBackendApplication.java
@@ -0,0 +1,13 @@
+package com.example.userDetails_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserDetailsBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserDetailsBackendApplication.class, args);
+	}
+
+}
---
+++ userDetails_backend/src/test/java/com/example/userDetails_backend/UserDetailsBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.userDetails_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserDetailsBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
